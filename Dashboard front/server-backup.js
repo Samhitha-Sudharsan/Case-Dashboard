814895eed9b6c9f80e6d866ad2a9c13ce13e11d6
Samhitha-Sudharsan <130522447+Samhitha-Sudharsan@users.noreply.github.com>
2024-07-09 18:00:48 +0530

    Update server-backup.js

--- Dashboard front/server-backup.js
+++ Dashboard front/server-backup.js
@@ -4,19 +4,13 @@ const mysql = require('mysql');
 const cors = require('cors');
 app.use(cors());
 
-const connection = mysql.createConnection({
-    host: 'justact-staging-db.cdzlwxwbylqw.ap-south-1.rds.amazonaws.com',
-    user: 'justact',
-    password: 'justact123!',
-    database: 'justact_staging'
-});
 
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 
     app.get('/api/get-parties-and-cases', (req, res) => {
-        const useremail = 'anandsingh039@gmail.com';
+        const useremail ;
         const fromDate = req.query.from; 
         const toDate = req.query.to; 
 
